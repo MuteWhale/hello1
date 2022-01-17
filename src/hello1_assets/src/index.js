5538be445531fa6cf7e12f6a2241a1958c76a20f
ClaraYang_ <38424534+MuteWhale@users.noreply.github.com>
2022-01-17 17:14:40 +0800

    Update index.js

--- src/hello1_assets/src/index.js
+++ src/hello1_assets/src/index.js
@@ -1,19 +1,44 @@
+import { HotModuleReplacementPlugin } from "webpack";
 import { hello1 } from "../../declarations/hello1";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+async function post(){
+   let post_button = document.getElementById("post");
+   let post_button = document.getElementById("post");
+   error.innerText = "";
+   post_button.disabled = true;
+   let textarea = document.getElementById("message");
+   let otp = document.getElementById("otp").value;
+   let text = textarea.value;
+   try{
+     await hello1.post(otp, text);
+     textarea.value = "";
+   } catch (err) {
+     console.log(err)
+     error.innerText = "Post Failed!";
+   }
+   await hello1.post(otp, text);
+   post_button.disabled = false;
+}
 
-  const name = document.getElementById("name").value.toString();
+var num_posts=0;
+async function load_posts(){
+    let posts_section = document.getElementById("posts");
+    posts_section.replaceChildren([]);
+    let posts = await hello1.posts();
+    if(num_posts == posts.length) return;
+    posts_section.replaceChildren([]);
+    num_posts = posts.length;
+    for (var i=0; i < posts.length; i++){
+        let post = document.createElement("p");
+        post.innerText = posts[i]
+        posts_section.appendChild(post)
+    }
 
-  button.setAttribute("disabled", true);
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello1.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+function load(){
+    let post_button = document.getElementById("post")
+    post_button.onclick = post;
+    load_posts()
+    setInterval(load_posts, 3000)
+}
